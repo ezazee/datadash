c8cf1b6ce3358e959df0103042c76b7f4ea08e11
ezazee <yukiirima123@gmail.com>
2025-02-24 16:07:56 +0700

    NextJS update to 14

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -9,11 +9,10 @@ export const metadata: Metadata = {
     title: 'PrimeReact Sakai',
     description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.',
     robots: { index: false, follow: false },
-    viewport: { initialScale: 1, width: 'device-width' },
+    metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'),
     openGraph: {
         type: 'website',
         title: 'PrimeReact SAKAI-REACT',
-        url: 'https://sakai.primereact.org/',
         description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.',
         images: ['https://www.primefaces.org/static/social/sakai-react.png'],
         ttl: 604800
@@ -21,6 +20,11 @@ export const metadata: Metadata = {
     icons: {
         icon: '/favicon.ico'
     }
+};
+
+export const viewport = {
+    initialScale: 1,
+    width: 'device-width'
 };
 
 export default function AppLayout({ children }: AppLayoutProps) {
